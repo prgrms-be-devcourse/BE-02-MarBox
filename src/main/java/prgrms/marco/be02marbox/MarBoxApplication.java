240995ddd84654c8f38f4dd7870fcfecf1bfdd1d
hyuk0309 <bahngeh@naver.com>
2022-06-16 19:05:52 +0900

    chore : 프로젝트 초기 세팅

---
+++ src/main/java/prgrms/marco/be02marbox/MarBoxApplication.java
@@ -0,0 +1,13 @@
+package prgrms.marco.be02marbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prgrms/marco/be02marbox/MarBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package prgrms.marco.be02marbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
