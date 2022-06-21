2b8e2087cefc0c2f9617ca75bd535fa5285d4334
hyuk0309 <bahngeh@naver.com>
2022-06-21 21:51:24 +0900

    test(UserIntegrationTest) : REST DOCS 용도 통합 테스트 작성

---
+++ src/test/java/prgrms/marco/be02marbox/domain/user/UserIntegrationTest.java
@@ -0,0 +1,64 @@
+package prgrms.marco.be02marbox.domain.user;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import prgrms.marco.be02marbox.domain.user.dto.UserSignUpReq;
+
+@SpringBootTest
+@AutoConfigureRestDocs
+@AutoConfigureTestDatabase
+@AutoConfigureMockMvc
+@Transactional
+class UserIntegrationTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Test
+	@DisplayName("사용자 회원 가입 API 성공")
+	void testSingUpApiSuccess() throws Exception {
+		//given
+		UserSignUpReq userSignUpReq = new UserSignUpReq(
+			"pang@mail.com",
+			"1234",
+			"pang",
+			Role.ROLE_ADMIN);
+
+		//when then
+		mockMvc.perform(post("/users/sign-up")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(userSignUpReq))
+				.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isCreated())
+			.andDo(document("user-sign-up",
+				requestFields(
+					fieldWithPath("email").type(JsonFieldType.STRING).description("사용자 이메일"),
+					fieldWithPath("password").type(JsonFieldType.STRING).description("사용자 비밀번호"),
+					fieldWithPath("name").type(JsonFieldType.STRING).description("사용자 이름"),
+					fieldWithPath("role").type(JsonFieldType.STRING).description("사용자 역할")
+				),
+				responseFields(
+					fieldWithPath("id").type(JsonFieldType.NUMBER).description("사용자 식별 번호")
+				)));
+	}
+}
