4e8d959ba04aeb9bb537e155445ab9fc4d3baca2
sdardew <sdardew@gmail.com>
2022-06-21 20:40:25 +0900

    test: createMovie() 메서드 테스트
    CreateMovieDto 인스턴스와 MovieService의 createMovie 메서드를 사용해서
    movie 테이블에 새로운 영화를 추가할 수 있다
    관리자는 추가한 영화의 데이터를 받는다

---
+++ src/test/java/prgrms/marco/be02marbox/domain/movie/service/MovieServiceTest.java
@@ -0,0 +1,44 @@
+package prgrms.marco.be02marbox.domain.movie.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import prgrms.marco.be02marbox.domain.movie.Genre;
+import prgrms.marco.be02marbox.domain.movie.LimitAge;
+import prgrms.marco.be02marbox.domain.movie.Movie;
+import prgrms.marco.be02marbox.domain.movie.dto.CreateMovieDto;
+
+@DataJpaTest
+@Import({MovieService.class})
+class MovieServiceTest {
+
+	@Autowired
+	MovieService movieService;
+
+	@Test
+	@DisplayName("movie를 추가할 수 있다")
+	void testCreateMovie() {
+		CreateMovieDto frozen = new CreateMovieDto("Frozen", LimitAge.CHILD, Genre.ANIMATION, 102, "frozen.png");
+		Movie movie = movieService.createMovie(frozen);
+		assertAll(
+			() -> assertThat(movie.getId()).isNotNull(),
+			() -> assertThat(movie.getName()).isEqualTo(frozen.getName()),
+			() -> assertThat(movie.getLimitAge()).isEqualTo(frozen.getLimitAge()),
+			() -> assertThat(movie.getGenre()).isEqualTo(frozen.getGenre()),
+			() -> assertThat(movie.getRunningTime()).isEqualTo(frozen.getRunningTime())
+		);
+	}
+
+	@Test
+	@DisplayName("test")
+	public void dummy() {
+		System.out.println("hi");
+	}
+}
