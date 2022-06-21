dde5dd17a5adbc69a9b38e6ef66dccc6c9bc52cf
wisehero10 <godjw96@gmail.com>
2022-06-21 23:04:30 +0900

    feat : TheaterConverter 작성

---
+++ src/main/java/prgrms/marco/be02marbox/domain/converter/TheaterConverter.java
@@ -0,0 +1,14 @@
+package prgrms.marco.be02marbox.domain.converter;
+
+import org.springframework.stereotype.Component;
+
+import prgrms.marco.be02marbox.domain.theater.Theater;
+import prgrms.marco.be02marbox.domain.theater.dto.RequestCreateTheater;
+
+@Component
+public class TheaterConverter {
+
+	public Theater getTheater(RequestCreateTheater request) {
+		return new Theater(request.getRegion(), request.getName());
+	}
+}
