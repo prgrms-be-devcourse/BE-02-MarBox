7fd966a08dd9dc7d9adc5cec5d2a15674a05e4f7
sdardew <sdardew@gmail.com>
2022-06-21 21:30:19 +0900

    feat: convertToMovie() 메서드 작성
    CreateMovieDto를 파라미터로 받아
    Movie 인스턴스로 변환하여 반환하는 메서드

---
+++ src/main/java/prgrms/marco/be02marbox/domain/movie/service/utils/MovieDtoConverter.java
@@ -0,0 +1,11 @@
+package prgrms.marco.be02marbox.domain.movie.service.utils;
+
+import prgrms.marco.be02marbox.domain.movie.Movie;
+import prgrms.marco.be02marbox.domain.movie.dto.CreateMovieDto;
+
+public class MovieDtoConverter {
+
+	public static Movie convertToMovie(CreateMovieDto createMovieDto) {
+		return new Movie(createMovieDto.getName(), createMovieDto.getLimitAge(), createMovieDto.getGenre(), createMovieDto.getRunningTime(), createMovieDto.getOriginalFileName());
+	}
+}
