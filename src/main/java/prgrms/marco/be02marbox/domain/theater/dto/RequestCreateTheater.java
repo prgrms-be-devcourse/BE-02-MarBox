6f5c808008d8a3f106af8c6053193b9792a54473
wisehero10 <godjw96@gmail.com>
2022-06-22 21:53:20 +0900

    refactor : Record CheckStyle 반영

--- src/main/java/prgrms/marco/be02marbox/domain/theater/dto/RequestCreateTheater.java
+++ src/main/java/prgrms/marco/be02marbox/domain/theater/dto/RequestCreateTheater.java
@@ -2,8 +2,9 @@ package prgrms.marco.be02marbox.domain.theater.dto;
 
 import javax.validation.constraints.NotBlank;
 
-public record RequestCreateTheater(@NotBlank(message = "지역명을 입력해주세요.") String region,
-								   @NotBlank(message = "영화관 이름을 입력해주세요.") String name) {
+public record RequestCreateTheater(
+	@NotBlank(message = "지역명을 입력해주세요.") String region,
+	@NotBlank(message = "영화관 이름을 입력해주세요.") String name) {
 
 	public RequestCreateTheater(String region, String name) {
 		this.region = region;
