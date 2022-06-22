d255c59004d4716d703a13bd7c4d60d1fb924ad1
yanJuicy <hanjub3@gmail.com>
2022-06-23 00:21:02 +0900

    test(ScheduleServiceTest.java): Schedule 생성 테스트 추가
    Schedule 생성이 성공하는 경우와 실패하는 경우 테스트를 추가했습니다.

--- src/test/java/prgrms/marco/be02marbox/domain/theater/service/ScheduleServiceTest.java
+++ src/test/java/prgrms/marco/be02marbox/domain/theater/service/ScheduleServiceTest.java
@@ -1,20 +1,22 @@
 package prgrms.marco.be02marbox.domain.theater.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.BDDMockito.given;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
+import prgrms.marco.be02marbox.domain.movie.Genre;
+import prgrms.marco.be02marbox.domain.movie.LimitAge;
 import prgrms.marco.be02marbox.domain.movie.Movie;
 import prgrms.marco.be02marbox.domain.movie.repository.MovieRepository;
 import prgrms.marco.be02marbox.domain.theater.Schedule;
@@ -23,44 +25,77 @@ import prgrms.marco.be02marbox.domain.theater.dto.ScheduleRecord;
 import prgrms.marco.be02marbox.domain.theater.repository.ScheduleRepository;
 import prgrms.marco.be02marbox.domain.theater.repository.TheaterRoomRepository;
 
-@ExtendWith(MockitoExtension.class)
+@DataJpaTest
+@Import(ScheduleService.class)
 class ScheduleServiceTest {
 
-	@Mock
+	@Autowired
 	private MovieRepository movieRepository;
 
-	@Mock
+	@Autowired
 	private TheaterRoomRepository theaterRoomRepository;
 
-	@Mock
+	@Autowired
 	private ScheduleRepository scheduleRepository;
 
-	@InjectMocks
+	@Autowired
 	private ScheduleService scheduleService;
+
+	private TheaterRoom theaterRoom;
+
+	private Movie movie;
+
+	@BeforeEach
+	void setup() {
+		theaterRoom = new TheaterRoom();
+		theaterRoomRepository.save(theaterRoom);
+		movie = new Movie("test", LimitAge.ADULT, Genre.ACTION, 100, "test");
+		movieRepository.save(movie);
+	}
+
+	@AfterEach
+	void clean() {
+		movieRepository.deleteAll();
+		theaterRoomRepository.deleteAll();
+	}
 
 	@Test
 	@DisplayName("스케줄 생성 성공 테스트")
 	void testCreateSchedule() {
-		ScheduleRecord scheduleRecord = new ScheduleRecord(1L, 1L, LocalDateTime.now(), LocalDateTime.now());
-
-		TheaterRoom theaterRoom = new TheaterRoom();
-		given(theaterRoomRepository.findById(anyLong())).willReturn(Optional.of(theaterRoom));
-
-		Movie movie = new Movie(null, null, null, null, null);
-		given(movieRepository.findById(anyLong())).willReturn(Optional.of(movie));
-
-		Schedule schedule = Schedule.builder()
-			.id(1L)
-			.theaterRoom(theaterRoom)
-			.movie(movie)
-			.startTime(scheduleRecord.startTime())
-			.endTime(scheduleRecord.endTime())
-			.build();
-		given(scheduleRepository.save(any(Schedule.class))).willReturn(schedule);
+		ScheduleRecord scheduleRecord = new ScheduleRecord(theaterRoom.getId(), movie.getId(), LocalDateTime.now(),
+			LocalDateTime.now());
 
 		Long id = scheduleService.createSchedule(scheduleRecord);
 
-		assertThat(id).isEqualTo(1L);
+		Optional<Schedule> findSchedule = scheduleRepository.findById(id);
+
+		assertAll(
+			() -> assertThat(findSchedule).isPresent(),
+			() -> assertThat(id).isEqualTo(findSchedule.get().getId()),
+			() -> assertThat(movie.getId()).isEqualTo(findSchedule.get().getMovie().getId())
+		);
+	}
+
+	@Test
+	@DisplayName("TheaterRoom이 없을 경우 스케줄 생성 실패")
+	void testCreateScheduleFail_TheaterRoom_No_Exists() {
+		ScheduleRecord scheduleRecord = new ScheduleRecord(100L, movie.getId(), LocalDateTime.now(),
+			LocalDateTime.now());
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			scheduleService.createSchedule(scheduleRecord);
+		});
+	}
+
+	@Test
+	@DisplayName("Movie가 없을 경우 스케줄 생성 실패")
+	void testCreateScheduleFail_Movie_No_Exists() {
+		ScheduleRecord scheduleRecord = new ScheduleRecord(theaterRoom.getId(), 100L, LocalDateTime.now(),
+			LocalDateTime.now());
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			scheduleService.createSchedule(scheduleRecord);
+		});
 	}
 
 }
