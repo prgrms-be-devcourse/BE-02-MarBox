928dbee4b45fb6b1675ba3212aa39185b2c989d2
wisehero10 <godjw96@gmail.com>
2022-06-22 21:45:37 +0900

    refactor : DTD 클래스 -> Record로 변환

--- src/main/java/prgrms/marco/be02marbox/domain/theater/dto/RequestCreateTheater.java
+++ src/main/java/prgrms/marco/be02marbox/domain/theater/dto/RequestCreateTheater.java
@@ -2,23 +2,21 @@ package prgrms.marco.be02marbox.domain.theater.dto;
 
 import javax.validation.constraints.NotBlank;
 
-public class RequestCreateTheater {
-
-	@NotBlank(message = "지역명을 입력해주세요.")
-	private final String region;
-	@NotBlank(message = "영화관 이름을 입력해주세요.")
-	private final String name;
+public record RequestCreateTheater(@NotBlank(message = "지역명을 입력해주세요.") String region,
+								   @NotBlank(message = "영화관 이름을 입력해주세요.") String name) {
 
 	public RequestCreateTheater(String region, String name) {
 		this.region = region;
 		this.name = name;
 	}
 
-	public String getRegion() {
+	@Override
+	public String region() {
 		return region;
 	}
 
-	public String getName() {
+	@Override
+	public String name() {
 		return name;
 	}
 }
