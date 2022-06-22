90854a6f79b9a5cc8c373a03943940bdcf20d267
wisehero10 <godjw96@gmail.com>
2022-06-22 21:45:21 +0900

    # refactor : 생성자 매개변수 타입 변경 String -> Region

--- src/main/java/prgrms/marco/be02marbox/domain/converter/TheaterConverter.java
+++ src/main/java/prgrms/marco/be02marbox/domain/converter/TheaterConverter.java
@@ -2,12 +2,14 @@ package prgrms.marco.be02marbox.domain.converter;
 
 import org.springframework.stereotype.Component;
 
+import prgrms.marco.be02marbox.domain.theater.Region;
 import prgrms.marco.be02marbox.domain.theater.Theater;
 import prgrms.marco.be02marbox.domain.theater.dto.RequestCreateTheater;
 
 @Component
 public class TheaterConverter {
 	public Theater getTheater(RequestCreateTheater request) {
-		return new Theater(request.getRegion(), request.getName());
+		return new Theater(
+			Region.valueOf(request.region()), request.name());
 	}
 }
