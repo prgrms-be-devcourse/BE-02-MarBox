26f9ec5a4cd1215b6924b824e2764f9ce0263acd
yanJuicy <hanjub3@gmail.com>
2022-06-23 16:49:38 +0900

    refactor: ScheduleConverter 의존성 제거 및 로직 분리
    ScheduleConverter 클래스에서 TheaterRoomRepository, MovieRepository 의존성을 분리해 ScheduleService에 다시 의존성을 추가했습니다.
    이에 따라서 TheaterRoom, Movie 객체를 찾는 로직도 Service 클래스로 이동했습니다.

--- src/main/java/prgrms/marco/be02marbox/domain/theater/service/ScheduleService.java
+++ src/main/java/prgrms/marco/be02marbox/domain/theater/service/ScheduleService.java
@@ -4,9 +4,13 @@ import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
+import prgrms.marco.be02marbox.domain.movie.Movie;
+import prgrms.marco.be02marbox.domain.movie.repository.MovieRepository;
 import prgrms.marco.be02marbox.domain.theater.Schedule;
+import prgrms.marco.be02marbox.domain.theater.TheaterRoom;
 import prgrms.marco.be02marbox.domain.theater.dto.RequestCreateSchedule;
 import prgrms.marco.be02marbox.domain.theater.repository.ScheduleRepository;
+import prgrms.marco.be02marbox.domain.theater.repository.TheaterRoomRepository;
 import prgrms.marco.be02marbox.domain.theater.service.utils.ScheduleConverter;
 
 @Service
@@ -14,16 +18,28 @@ public class ScheduleService {
 
 	private final ScheduleRepository scheduleRepository;
 	private final ScheduleConverter converter;
+	private final TheaterRoomRepository theaterRoomRepository;
+	private final MovieRepository movieRepository;
 
 	public ScheduleService(ScheduleRepository scheduleRepository,
-		ScheduleConverter converter) {
+		ScheduleConverter converter,
+		TheaterRoomRepository theaterRoomRepository,
+		MovieRepository movieRepository) {
 		this.scheduleRepository = scheduleRepository;
 		this.converter = converter;
+		this.theaterRoomRepository = theaterRoomRepository;
+		this.movieRepository = movieRepository;
 	}
 
 	@Transactional
 	public Long createSchedule(RequestCreateSchedule requestCreateSchedule) {
-		Schedule schedule = converter.convertFromRequestCreateScheduleToSchdeule(requestCreateSchedule);
+		TheaterRoom theaterRoom = theaterRoomRepository.findById(requestCreateSchedule.theaterRoomId())
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상영관 ID"));
+		Movie movie = movieRepository.findById(requestCreateSchedule.movieId())
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 영화 ID"));
+
+		Schedule schedule = converter.convertFromRequestCreateScheduleToschdeule(requestCreateSchedule, theaterRoom,
+			movie);
 
 		Schedule savedSchedule = scheduleRepository.save(schedule);
 
--- src/test/java/prgrms/marco/be02marbox/domain/theater/service/ScheduleServiceMockTest.java
+++ src/test/java/prgrms/marco/be02marbox/domain/theater/service/ScheduleServiceMockTest.java
@@ -2,9 +2,11 @@ package prgrms.marco.be02marbox.domain.theater.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,14 +16,22 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import prgrms.marco.be02marbox.domain.movie.Movie;
+import prgrms.marco.be02marbox.domain.movie.repository.MovieRepository;
 import prgrms.marco.be02marbox.domain.theater.Schedule;
 import prgrms.marco.be02marbox.domain.theater.TheaterRoom;
 import prgrms.marco.be02marbox.domain.theater.dto.RequestCreateSchedule;
 import prgrms.marco.be02marbox.domain.theater.repository.ScheduleRepository;
+import prgrms.marco.be02marbox.domain.theater.repository.TheaterRoomRepository;
 import prgrms.marco.be02marbox.domain.theater.service.utils.ScheduleConverter;
 
 @ExtendWith(MockitoExtension.class)
 class ScheduleServiceMockTest {
+
+	@Mock
+	private MovieRepository movieRepository;
+
+	@Mock
+	private TheaterRoomRepository theaterRoomRepository;
 
 	@Mock
 	private ScheduleRepository scheduleRepository;
@@ -39,8 +49,10 @@ class ScheduleServiceMockTest {
 			LocalDateTime.now());
 
 		TheaterRoom theaterRoom = new TheaterRoom();
+		given(theaterRoomRepository.findById(anyLong())).willReturn(Optional.of(theaterRoom));
 
 		Movie movie = new Movie(null, null, null, null, null);
+		given(movieRepository.findById(anyLong())).willReturn(Optional.of(movie));
 
 		Schedule schedule = Schedule.builder()
 			.theaterRoom(theaterRoom)
@@ -49,7 +61,9 @@ class ScheduleServiceMockTest {
 			.endTime(requestCreateSchedule.endTime())
 			.build();
 		given(
-			scheduleConverter.convertFromRequestCreateScheduleToSchdeule(any(RequestCreateSchedule.class))).willReturn(
+			scheduleConverter.convertFromRequestCreateScheduleToschdeule(any(RequestCreateSchedule.class),
+				any(TheaterRoom.class),
+				any(Movie.class))).willReturn(
 			schedule);
 		given(scheduleRepository.save(any(Schedule.class))).willReturn(schedule);
 
