711e86bffb4612436c29f94e66387d1d35848a8a
sdardew <sdardew@gmail.com>
2022-06-23 13:59:04 +0900

    fix: converter 컨벤션에 맞춰 변경

--- src/main/java/prgrms/marco/be02marbox/domain/movie/service/utils/MovieConverter.java
+++ src/main/java/prgrms/marco/be02marbox/domain/movie/service/utils/MovieConverter.java
@@ -1,15 +1,18 @@
 package prgrms.marco.be02marbox.domain.movie.service.utils;
 
+import org.springframework.stereotype.Component;
+
 import prgrms.marco.be02marbox.domain.movie.Movie;
-import prgrms.marco.be02marbox.domain.movie.dto.CreateMovie;
+import prgrms.marco.be02marbox.domain.movie.dto.RequestCreateMovie;
 
-public class MovieDtoConverter {
+@Component
+public class MovieConverter {
 
-	public static Movie convertToMovie(CreateMovie createMovie) {
-		return new Movie(createMovie.name(),
-			createMovie.limitAge(),
-			createMovie.genre(),
-			createMovie.runningTime(),
-			createMovie.originalFileName());
+	public Movie convertFromRequestCreateMovieToMovie(RequestCreateMovie requestCreateMovie) {
+		return new Movie(requestCreateMovie.name(),
+			requestCreateMovie.limitAge(),
+			requestCreateMovie.genre(),
+			requestCreateMovie.runningTime(),
+			requestCreateMovie.originalFileName());
 	}
 }
