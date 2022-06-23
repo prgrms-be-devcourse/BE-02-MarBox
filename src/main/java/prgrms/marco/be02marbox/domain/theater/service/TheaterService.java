db3cdb15a37a55b4c2a393e855eff65eb7907ded
wisehero10 <godjw96@gmail.com>
2022-06-23 14:16:41 +0900

    refactor : 컨버터 이름 변경 및 사용 위치 새로운 이름으로 갱신

--- src/main/java/prgrms/marco/be02marbox/domain/theater/service/TheaterService.java
+++ src/main/java/prgrms/marco/be02marbox/domain/theater/service/TheaterService.java
@@ -3,7 +3,7 @@ package prgrms.marco.be02marbox.domain.theater.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import prgrms.marco.be02marbox.domain.converter.TheaterConverter;
+import prgrms.marco.be02marbox.domain.theater.service.utils.TheaterConverter;
 import prgrms.marco.be02marbox.domain.theater.Theater;
 import prgrms.marco.be02marbox.domain.theater.dto.RequestCreateTheater;
 import prgrms.marco.be02marbox.domain.theater.repository.TheaterRepository;
@@ -21,7 +21,7 @@ public class TheaterService {
 
 	@Transactional
 	public Long createTheater(RequestCreateTheater request) {
-		Theater newTheater = theaterConverter.getTheater(request);
+		Theater newTheater = theaterConverter.convertFromRequestCreateTheaterToTheater(request);
 		Theater savedTheater = theaterRepository.save(newTheater);
 		return savedTheater.getId();
 	}
--- src/test/java/prgrms/marco/be02marbox/domain/theater/service/TheaterServiceTest.java
+++ src/test/java/prgrms/marco/be02marbox/domain/theater/service/TheaterServiceTest.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
-import prgrms.marco.be02marbox.domain.converter.TheaterConverter;
+import prgrms.marco.be02marbox.domain.theater.service.utils.TheaterConverter;
 import prgrms.marco.be02marbox.domain.theater.Region;
 import prgrms.marco.be02marbox.domain.theater.Theater;
 import prgrms.marco.be02marbox.domain.theater.dto.RequestCreateTheater;
