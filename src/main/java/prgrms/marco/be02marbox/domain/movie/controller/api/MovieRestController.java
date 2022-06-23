d2359ec3b5f5fbcf56942d4349ef4852ce472bf7
sdardew <sdardew@gmail.com>
2022-06-23 18:25:49 +0900

    feat: movies 조회 기능 추가

--- src/main/java/prgrms/marco/be02marbox/domain/movie/controller/api/MovieRestController.java
+++ src/main/java/prgrms/marco/be02marbox/domain/movie/controller/api/MovieRestController.java
@@ -1,9 +1,24 @@
 package prgrms.marco.be02marbox.domain.movie.controller.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/movies")
+import prgrms.marco.be02marbox.domain.movie.service.MovieService;
+
+@RequestMapping("/api/movies")
 @RestController
 public class MovieRestController {
+
+	private final MovieService movieService;
+
+	public MovieRestController(MovieService movieService) {
+		this.movieService = movieService;
+	}
+
+	@GetMapping
+	public void getMovies(@RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "10") int size) {
+		movieService.getMovies(page, size);
+	}
 }
