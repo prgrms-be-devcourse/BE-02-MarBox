b13973ab40f09dce157e5183df9ad87046a49d0c
taesan <pawer02023@gmail.com>
2022-06-23 17:41:35 +0900

    refactor: save 로직 변경
    - TheaterRoom이 seat의 생명주기를 관리할 수 있도록 의도
    - Transactional 추가

--- src/main/java/prgrms/marco/be02marbox/domain/theater/service/TheaterRoomService.java
+++ src/main/java/prgrms/marco/be02marbox/domain/theater/service/TheaterRoomService.java
@@ -7,14 +7,15 @@ import java.util.List;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import prgrms.marco.be02marbox.domain.theater.Seat;
 import prgrms.marco.be02marbox.domain.theater.Theater;
 import prgrms.marco.be02marbox.domain.theater.TheaterRoom;
-import prgrms.marco.be02marbox.domain.theater.converter.SeatConverter;
-import prgrms.marco.be02marbox.domain.theater.dto.TheaterRoomRequestDto;
+import prgrms.marco.be02marbox.domain.theater.dto.RequestCreateTheaterRoom;
 import prgrms.marco.be02marbox.domain.theater.repository.TheaterRepository;
 import prgrms.marco.be02marbox.domain.theater.repository.TheaterRoomRepository;
+import prgrms.marco.be02marbox.domain.theater.service.utils.SeatConverter;
 
 @Service
 public class TheaterRoomService {
@@ -33,17 +34,17 @@ public class TheaterRoomService {
 		this.seatConverter = seatConverter;
 	}
 
-	public Long save(TheaterRoomRequestDto requestDto) {
-		Theater theater = theaterRepository.findById(requestDto.getTheaterId())
+	@Transactional
+	public Long save(RequestCreateTheaterRoom requestCreateTheaterRoom) {
+		Theater theater = theaterRepository.findById(requestCreateTheaterRoom.theaterId())
 			.orElseThrow(() -> new EntityNotFoundException(NOT_FOUND_THEATER_ERR));
 
-		List<Seat> seats = requestDto.getSeatRequestDtos().stream()
-			.distinct()
-			.map(seatConverter::convertSeat)
-			.collect(toList());
-
-		TheaterRoom newTheaterRoom = new TheaterRoom(theater, requestDto.getName(), seats);
+		TheaterRoom newTheaterRoom = new TheaterRoom(theater, requestCreateTheaterRoom.name());
 		TheaterRoom savedTheaterRoom = theaterRoomRepository.save(newTheaterRoom);
+		List<Seat> seatList = requestCreateTheaterRoom.requestCreateSeats().stream().map(requestCreateSeat ->
+			seatConverter.convertFromRequestSeatToSeat(savedTheaterRoom, requestCreateSeat)
+		).collect(toList());
+		savedTheaterRoom.addSeats(seatList);
 		return savedTheaterRoom.getId();
 	}
 }
--- src/test/java/prgrms/marco/be02marbox/domain/theater/service/TheaterRoomServiceTest.java
+++ src/test/java/prgrms/marco/be02marbox/domain/theater/service/TheaterRoomServiceTest.java
@@ -3,9 +3,10 @@ package prgrms.marco.be02marbox.domain.theater.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import javax.persistence.EntityNotFoundException;
 
@@ -20,12 +21,12 @@ import org.springframework.context.annotation.Import;
 import prgrms.marco.be02marbox.domain.theater.Seat;
 import prgrms.marco.be02marbox.domain.theater.Theater;
 import prgrms.marco.be02marbox.domain.theater.TheaterRoom;
-import prgrms.marco.be02marbox.domain.theater.converter.SeatConverter;
-import prgrms.marco.be02marbox.domain.theater.dto.SeatRequestDto;
-import prgrms.marco.be02marbox.domain.theater.dto.TheaterRoomRequestDto;
+import prgrms.marco.be02marbox.domain.theater.dto.RequestCreateSeat;
+import prgrms.marco.be02marbox.domain.theater.dto.RequestCreateTheaterRoom;
 import prgrms.marco.be02marbox.domain.theater.repository.SeatRepository;
 import prgrms.marco.be02marbox.domain.theater.repository.TheaterRepository;
 import prgrms.marco.be02marbox.domain.theater.repository.TheaterRoomRepository;
+import prgrms.marco.be02marbox.domain.theater.service.utils.SeatConverter;
 
 @DataJpaTest
 @Import({TheaterRoomService.class, SeatConverter.class})
@@ -41,25 +42,26 @@ class TheaterRoomServiceTest {
 	SeatRepository seatRepository;
 
 	private Theater theater = new Theater();
-	private List<SeatRequestDto> seatRequestDtos = new ArrayList<>();
+	private Set<RequestCreateSeat> requestCreateSeats = new HashSet<>();
 
 	@BeforeEach
 	void init() {
-		seatRequestDtos.add(new SeatRequestDto(0, 0));
+		requestCreateSeats.add(new RequestCreateSeat(0, 0));
 		theaterRepository.save(theater);
 	}
 
 	@AfterEach
 	void clear() {
-		seatRequestDtos.clear();
+		requestCreateSeats.clear();
 	}
 
 	@Test
 	@DisplayName("새로운 상영관을 추가할 수 있다.")
 	void testSave() {
-		TheaterRoomRequestDto requestDto = new TheaterRoomRequestDto(theater.getId(), "A관", seatRequestDtos);
-		int expectTotalCount = seatRequestDtos.size();
-		Long savedId = theaterRoomService.save(requestDto);
+		RequestCreateTheaterRoom requestCreateTheaterRoom = new RequestCreateTheaterRoom(theater.getId(), "A관",
+			requestCreateSeats);
+		int expectTotalCount = requestCreateSeats.size();
+		Long savedId = theaterRoomService.save(requestCreateTheaterRoom);
 
 		Optional<TheaterRoom> findTheaterRoom = theaterRoomRepository.findById(savedId);
 		List<Seat> findSeats = seatRepository.findByTheaterRoomId(savedId);
@@ -73,16 +75,17 @@ class TheaterRoomServiceTest {
 	@Test
 	@DisplayName("하나의 상영관에는 동일한 row, col을 가진 좌석이 존재할 수 없다.")
 	void testSave_distinct() {
-		seatRequestDtos.add(new SeatRequestDto(0, 1));
-		seatRequestDtos.add(new SeatRequestDto(0, 2));
-		seatRequestDtos.add(new SeatRequestDto(0, 1));
-		seatRequestDtos.add(new SeatRequestDto(0, 2));
-		seatRequestDtos.add(new SeatRequestDto(0, 1));
-		seatRequestDtos.add(new SeatRequestDto(0, 2));
+		requestCreateSeats.add(new RequestCreateSeat(0, 1));
+		requestCreateSeats.add(new RequestCreateSeat(0, 2));
+		requestCreateSeats.add(new RequestCreateSeat(0, 1));
+		requestCreateSeats.add(new RequestCreateSeat(0, 2));
+		requestCreateSeats.add(new RequestCreateSeat(0, 1));
+		requestCreateSeats.add(new RequestCreateSeat(0, 2));
 
-		TheaterRoomRequestDto requestDto = new TheaterRoomRequestDto(theater.getId(), "A관", seatRequestDtos);
+		RequestCreateTheaterRoom requestCreateTheaterRoom = new RequestCreateTheaterRoom(theater.getId(), "A관",
+			requestCreateSeats);
 		int expectTotalCount = 3;
-		Long savedId = theaterRoomService.save(requestDto);
+		Long savedId = theaterRoomService.save(requestCreateTheaterRoom);
 		List<Seat> findSeats = seatRepository.findByTheaterRoomId(savedId);
 
 		assertThat(findSeats).hasSize(expectTotalCount);
@@ -91,8 +94,9 @@ class TheaterRoomServiceTest {
 	@Test
 	@DisplayName("존재하지 않는 Theater의 요청은 EntityNotFoundException 발생")
 	void testSave_entityNotFoundException() {
-		TheaterRoomRequestDto requestDto = new TheaterRoomRequestDto(-1L, "A관", seatRequestDtos);
+		RequestCreateTheaterRoom requestCreateTheaterRoom = new RequestCreateTheaterRoom(-1L, "A관", requestCreateSeats);
 
-		assertThrows(EntityNotFoundException.class, () -> theaterRoomService.save(requestDto));
+		assertThrows(EntityNotFoundException.class, () -> theaterRoomService.save(requestCreateTheaterRoom));
 	}
 }
+
