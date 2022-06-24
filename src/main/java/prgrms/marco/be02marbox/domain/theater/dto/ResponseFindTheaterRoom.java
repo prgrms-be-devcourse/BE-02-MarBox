b96a34310f6d09410ea38cb00085b4e943bc38fb
taesan <pawer02023@gmail.com>
2022-06-24 16:00:21 +0900

    refactor: theater 변수명 변경
    - 응답 변수 명 가독성을 위해 변경

--- src/main/java/prgrms/marco/be02marbox/domain/theater/dto/ResponseFindTheaterRoom.java
+++ src/main/java/prgrms/marco/be02marbox/domain/theater/dto/ResponseFindTheaterRoom.java
@@ -2,7 +2,7 @@ package prgrms.marco.be02marbox.domain.theater.dto;
 
 import javax.validation.constraints.NotNull;
 
-public record ResponseFindTheaterRoom(ResponseFindTheater responseFindTheater,
+public record ResponseFindTheaterRoom(ResponseFindTheater theater,
 	@NotNull String theaterRoomName,
 	@NotNull int totalCount) {
 }
