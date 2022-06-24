e9d2e58d53bd64b38aa99aff780d19755bcc036d
wisehero10 <godjw96@gmail.com>
2022-06-24 19:27:05 +0900

    chore : Region 추가

--- src/main/java/prgrms/marco/be02marbox/domain/theater/Region.java
+++ src/main/java/prgrms/marco/be02marbox/domain/theater/Region.java
@@ -1,5 +1,16 @@
 package prgrms.marco.be02marbox.domain.theater;
 
 public enum Region {
-	SEOUL, BUSAN;
+	SEOUL,
+	INCHEON,
+	BUSAN,
+	ULSAN,
+	DAEGU,
+	DAEJEON,
+	GWANGJU,
+	GYEONGGI,
+	CHUNGCHEONG,
+	GANGWON,
+	GYEONGSANG,
+	JEOLLA,
 }
