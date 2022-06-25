0363c1e8044429322337e0856495c5a1305572f1
sdardew <sdardew@gmail.com>
2022-06-25 17:43:11 +0900

    test: getMovies() 테스트

--- src/test/java/prgrms/marco/be02marbox/domain/movie/controller/api/MovieRestControllerTest.java
+++ src/test/java/prgrms/marco/be02marbox/domain/movie/controller/api/MovieRestControllerTest.java
@@ -9,10 +9,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,12 +23,17 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+import prgrms.marco.be02marbox.config.WebSecurityConfigure;
 import prgrms.marco.be02marbox.domain.movie.Genre;
 import prgrms.marco.be02marbox.domain.movie.LimitAge;
 import prgrms.marco.be02marbox.domain.movie.Movie;
 import prgrms.marco.be02marbox.domain.movie.service.MovieService;
 
-@WebMvcTest(MovieRestController.class)
+@WebMvcTest(controllers = MovieRestController.class,
+	excludeFilters = {
+		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigure.class)
+	}
+)
 class MovieRestControllerTest {
 
 	@Autowired
@@ -40,7 +48,7 @@ class MovieRestControllerTest {
 			.build();
 	}
 
-/*	@Test
+	@Test
 	@WithMockUser(roles = "ADMIN")
 	void testGetMovies() throws Exception {
 		MultiValueMap<String, String> multiValueMap = new LinkedMultiValueMap<>();
@@ -57,5 +65,5 @@ class MovieRestControllerTest {
 					.characterEncoding("UTF-8"))
 			.andExpect(status().isOk())
 			.andDo(print());
-	}*/
+	}
 }
