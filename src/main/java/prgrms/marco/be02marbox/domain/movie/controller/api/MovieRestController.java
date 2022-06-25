b839b40b138ea5910f00113cb457ec249f834ef5
sdardew <sdardew@gmail.com>
2022-06-25 15:03:23 +0900

    chore: 컨벤션 맞춰 변경

--- src/main/java/prgrms/marco/be02marbox/domain/movie/controller/api/MovieRestController.java
+++ src/main/java/prgrms/marco/be02marbox/domain/movie/controller/api/MovieRestController.java
@@ -1,10 +1,13 @@
 package prgrms.marco.be02marbox.domain.movie.controller.api;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import prgrms.marco.be02marbox.domain.movie.dto.ResponseGetMovies;
 import prgrms.marco.be02marbox.domain.movie.service.MovieService;
 
 @RequestMapping("/api/movies")
@@ -17,8 +20,9 @@ public class MovieRestController {
 		this.movieService = movieService;
 	}
 
-	@GetMapping
-	public void getMovies(@RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "10") int size) {
-		movieService.getMovies(page, size);
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<ResponseGetMovies> getMovies(@RequestParam(name = "page", defaultValue = "0") int page,
+		@RequestParam(name = "size", defaultValue = "10") int size) {
+		return ResponseEntity.ok().body(new ResponseGetMovies(movieService.getMovies(page, size)));
 	}
 }
