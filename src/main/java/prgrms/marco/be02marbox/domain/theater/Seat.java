62f2590ba1559cf15111a8dae54850ae79ff8ffb
hyuk0309 <bahngeh@naver.com>
2022-06-27 16:48:44 +0900

    chore(seat.java) : 테이블 컬럼명 수정
    theater_room_row를 seat_row
    theater_room_col을 seat_col

--- src/main/java/prgrms/marco/be02marbox/domain/theater/Seat.java
+++ src/main/java/prgrms/marco/be02marbox/domain/theater/Seat.java
@@ -18,7 +18,7 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 
 @Entity
 @Table(name = "seat", uniqueConstraints = {
-	@UniqueConstraint(columnNames = {"theater_room_id", "theater_room_row", "theater_room_col"})
+	@UniqueConstraint(columnNames = {"theater_room_id", "seat_row", "seat_col"})
 })
 public class Seat {
 
@@ -31,10 +31,10 @@ public class Seat {
 	@JoinColumn(name = "theater_room_id")
 	private TheaterRoom theaterRoom;
 
-	@Column(name = "theater_room_row")
+	@Column(name = "seat_row")
 	private Integer row;
 
-	@Column(name = "theater_room_col")
+	@Column(name = "seat_col")
 	private Integer column;
 
 	protected Seat() {
