09470eb3d545028e7e92efa068151cc9cf80635e
hyuk0309 <bahngeh@naver.com>
2022-06-27 15:18:26 +0900

    docs(UserService.class) : UserService 클래스 메서드에 설명 주석 추가

--- src/main/java/prgrms/marco/be02marbox/domain/user/service/UserService.java
+++ src/main/java/prgrms/marco/be02marbox/domain/user/service/UserService.java
@@ -23,6 +23,16 @@ public class UserService {
 		this.userRepository = userRepository;
 	}
 
+	/**
+	 * 사용자 정보를 받아, User 엔티티를 만들고 DB에 저장한다.
+	 * @param email
+	 * @param password
+	 * @param name
+	 * @param role
+	 * @return 사용자 아이디
+	 * @throws DuplicateEmailException - 입력 받은 이메일이 DB에 존재하는 경우
+	 * @throws DuplicateNameException - 입력 받은 이름이 DB에 존재하는 경우
+	 */
 	@Transactional
 	public Long create(String email, String password, String name, Role role) {
 		userRepository.findByEmail(email)
@@ -40,6 +50,14 @@ public class UserService {
 		return savedUser.getId();
 	}
 
+	/**
+	 * 이메일과 비밀번호를 받아, 존재하는 사용자인지 확인한다.
+	 * @param email
+	 * @param password
+	 * @return ResponseLoginUser
+	 * @throws InvalidEmailException 이메일이 DB에 존재하지 않는 경우
+	 * @throws org.springframework.security.authentication.BadCredentialsException 비밀번호가 틀린 경우
+	 */
 	public ResponseLoginUser login(String email, String password) {
 		User user = userRepository.findByEmail(email)
 			.orElseThrow(() -> new InvalidEmailException(INVALID_EMAIL_EXP_MSG));
