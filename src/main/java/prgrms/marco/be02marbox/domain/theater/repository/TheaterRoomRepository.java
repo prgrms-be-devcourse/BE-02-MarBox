ca823b59021c2b597f7261d15b30ec7d9883ac8c
yanJuicy <hanjub3@gmail.com>
2022-06-28 16:02:42 +0900

    feat(TheaterRoomRepository): TheateId로 TheaterRoom을 찾는 메소드 추가

--- src/main/java/prgrms/marco/be02marbox/domain/theater/repository/TheaterRoomRepository.java
+++ src/main/java/prgrms/marco/be02marbox/domain/theater/repository/TheaterRoomRepository.java
@@ -1,8 +1,12 @@
 package prgrms.marco.be02marbox.domain.theater.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import prgrms.marco.be02marbox.domain.theater.TheaterRoom;
 
 public interface TheaterRoomRepository extends JpaRepository<TheaterRoom, Long> {
+
+	List<TheaterRoom> findAllByTheaterId(Long theaterId);
 }
