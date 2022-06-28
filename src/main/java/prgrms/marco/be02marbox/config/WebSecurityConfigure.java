cc51ed76e141a8e332ebe334f6d08e10b2db31fd
hyuk0309 <bahngeh@naver.com>
2022-06-28 18:50:55 +0900

    feat(WebSecurityConfigure.java) : Spring Security 설정 추가
    JwtAuthenticationFilter를 FilterChain에 등록
    API 역할 부여

--- src/main/java/prgrms/marco/be02marbox/config/WebSecurityConfigure.java
+++ src/main/java/prgrms/marco/be02marbox/config/WebSecurityConfigure.java
@@ -7,8 +7,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 
 import prgrms.marco.be02marbox.domain.user.jwt.Jwt;
+import prgrms.marco.be02marbox.domain.user.jwt.JwtAuthenticationFilter;
 import prgrms.marco.be02marbox.domain.user.jwt.JwtAuthenticationProvider;
 import prgrms.marco.be02marbox.domain.user.service.UserService;
 
@@ -41,11 +43,17 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
 		return super.authenticationManagerBean();
 	}
 
+	public JwtAuthenticationFilter jwtAuthenticationFilter() {
+		Jwt jwt = getApplicationContext().getBean(Jwt.class);
+		return new JwtAuthenticationFilter(jwtConfigure.header(), jwt);
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-			.anyRequest().permitAll()
+			.antMatchers("/users/sign-up", "/users/sign-in").permitAll()
+			.anyRequest().hasAnyRole("ADMIN")
 			.and()
 
 			.formLogin()
@@ -67,6 +75,9 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
 			.disable()
 
 			.sessionManagement()
-			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+
+			.addFilterAfter(jwtAuthenticationFilter(), SecurityContextPersistenceFilter.class);
 	}
 }
