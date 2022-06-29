5a5988d85a3dea59391ae5baa3700b58d7321ecc
wisehero10 <godjw96@gmail.com>
2022-06-29 22:34:53 +0900

    feat : TicketRepository -> findTicketByUserId 메서드 추가

--- src/main/java/prgrms/marco/be02marbox/domain/reservation/repository/TicketRepository.java
+++ src/main/java/prgrms/marco/be02marbox/domain/reservation/repository/TicketRepository.java
@@ -1,8 +1,11 @@
 package prgrms.marco.be02marbox.domain.reservation.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import prgrms.marco.be02marbox.domain.reservation.Ticket;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+	Optional<Ticket> findTicketByUserId(Long userId);
 }
