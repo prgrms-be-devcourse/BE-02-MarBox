4b649a67b6d3f36742740b5a49ead16fc321db9b
wisehero10 <godjw96@gmail.com>
2022-06-29 22:27:02 +0900

    feat : 티켓 정보 관련 에러 메세지 추가

--- src/main/java/prgrms/marco/be02marbox/domain/exception/custom/Message.java
+++ src/main/java/prgrms/marco/be02marbox/domain/exception/custom/Message.java
@@ -5,7 +5,8 @@ public enum Message {
 	DUPLICATE_NAME_EXP_MSG("이미 존재하는 이름입니다."),
 	INVALID_EMAIL_EXP_MSG("존재하지 않는 이메일 입니다."),
 	ACCESS_DENIED_EXP_MSG("API 접근 권한이 없습니다."),
-	INVALID_THEATER_EXP_MSG("존재하지 않는 극장 ID 입니다.");
+	INVALID_THEATER_EXP_MSG("존재하지 않는 극장 ID 입니다."),
+	NO_USER_TICKET_INFO_EXP_MSG("존재하지 않는 유저의 티켓 정보입니다.");
 
 	private final String message;
 
