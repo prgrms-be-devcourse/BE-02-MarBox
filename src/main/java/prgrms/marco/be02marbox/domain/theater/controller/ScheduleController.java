e27ab7bda9593ee61e7dcdd44446a0e7df45b6b8
yanJuicy <hanjub3@gmail.com>
2022-06-30 08:17:55 +0900

    refactor(ScheduleController): 한 메소드에서 요청 파라미터에 따라 다른 서비스 메서드를 호출하도록 변경
    기존에는 @RequestParam의 타입의, 갯수를 다르게 해서 메소드를 분리하려 했으나 @GetMapping이 결국 같아져서
    하나의 메소드에서 처리해야 함

--- src/main/java/prgrms/marco/be02marbox/domain/theater/controller/ScheduleController.java
+++ src/main/java/prgrms/marco/be02marbox/domain/theater/controller/ScheduleController.java
@@ -2,6 +2,7 @@ package prgrms.marco.be02marbox.domain.theater.controller;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -14,9 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import prgrms.marco.be02marbox.domain.movie.dto.ResponseFindCurrentMovie;
+import prgrms.marco.be02marbox.domain.movie.dto.ResponseFindMovie;
 import prgrms.marco.be02marbox.domain.theater.dto.RequestCreateSchedule;
-import prgrms.marco.be02marbox.domain.theater.dto.ResponseFindMovieListAndDateList;
+import prgrms.marco.be02marbox.domain.theater.dto.ResponseFindSchedule;
 import prgrms.marco.be02marbox.domain.theater.service.ScheduleService;
 
 @RestController
@@ -37,15 +38,21 @@ public class ScheduleController {
 	}
 
 	@GetMapping("/current-movies")
-	public ResponseEntity<List<ResponseFindCurrentMovie>> getCurrentMovieList() {
-		List<ResponseFindCurrentMovie> currentMovieList = scheduleService.findShowingMovieList();
+	public ResponseEntity<List<ResponseFindMovie>> getCurrentMovieList() {
+		List<ResponseFindMovie> currentMovieList = scheduleService.findShowingMovieList();
 		return ResponseEntity.ok().body(currentMovieList);
 	}
 
 	@GetMapping
-	public ResponseEntity<ResponseFindMovieListAndDateList> getMovieAndDateListInOneTheater(
-		@RequestParam Long theaterId) {
-		return ResponseEntity.ok().body(scheduleService.findMovieListAndDateListByTheaterId(theaterId));
+	public ResponseEntity<ResponseFindSchedule> getSchedulesByRequestParam(
+		@RequestParam(required = false) Long movieId,
+		@RequestParam(required = false) Long theaterId,
+		@RequestParam(required = false) LocalDate date) {
+		if (movieId == null && theaterId != null && date == null) {
+			return ResponseEntity.ok().body(scheduleService.findMovieListAndDateListByTheaterId(theaterId));
+		}
+
+		return ResponseEntity.ok().body(new ResponseFindSchedule(List.of(), List.of(), List.of(), List.of()));
 	}
 
 }
