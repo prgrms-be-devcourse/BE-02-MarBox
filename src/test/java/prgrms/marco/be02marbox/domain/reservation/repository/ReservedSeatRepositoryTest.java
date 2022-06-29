ef4eaf70f37861e36df4d7d432236b4a7bd16b38
taesan <pawer02023@gmail.com>
2022-06-30 08:56:43 +0900

    chore: 피드백 반영
    - 함수명 변경

--- src/test/java/prgrms/marco/be02marbox/domain/reservation/repository/ReservedSeatRepositoryTest.java
+++ src/test/java/prgrms/marco/be02marbox/domain/reservation/repository/ReservedSeatRepositoryTest.java
@@ -2,6 +2,7 @@ package prgrms.marco.be02marbox.domain.reservation.repository;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static prgrms.marco.be02marbox.domain.reservation.ReservedSeat.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,7 +34,7 @@ class ReservedSeatRepositoryTest extends RepositoryTestUtil {
 				ReservedSeat getReservedSeat = findReservedSeat.get();
 				StringBuilder makeId = new StringBuilder()
 					.append(reservedSeat.getTicket().getSchedule().getId())
-					.append("_")
+					.append(ID_SEPARATOR)
 					.append(reservedSeat.getSeat().getId());
 				assertThat(getReservedSeat.getId()).isEqualTo(makeId.toString());
 			}
@@ -44,9 +45,9 @@ class ReservedSeatRepositoryTest extends RepositoryTestUtil {
 	@CsvSource({"13", "5"})
 	@DisplayName("like 조건을 통해 스케줄 id 에 예메된 좌석정보를 조회한다.")
 	void testSearchByIdStartsWith(int expectCount) {
-		Schedule schedule = saveSameScheduleSeatList(expectCount);
+		Schedule schedule = saveReservedSeatMultiSeat(expectCount);
 
-		String paramId = getParamId(schedule);
+		String paramId = makeFindByScheduleParam(schedule.getId());
 		List<ReservedSeat> reservedSeats = reservedSeatRepository.searchByIdStartsWith(paramId);
 
 		assertThat(reservedSeats).hasSize(expectCount);
@@ -56,7 +57,7 @@ class ReservedSeatRepositoryTest extends RepositoryTestUtil {
 	@CsvSource({"__", "*", "?"})
 	@DisplayName("like 조건 잘못된 id형식으로 조회하는 경우")
 	void testSearchByIdStartsWithBadParam(String separator) {
-		Schedule schedule = saveSameScheduleSeatList(1);
+		Schedule schedule = saveReservedSeatMultiSeat(1);
 		String paramId = new StringBuilder().append(schedule.getId()).append(separator).toString();
 		List<ReservedSeat> reservedSeats = reservedSeatRepository.searchByIdStartsWith(paramId);
 
@@ -66,14 +67,14 @@ class ReservedSeatRepositoryTest extends RepositoryTestUtil {
 	@Test
 	@DisplayName("저장되지 않은 {schedule_id})_ 로 조회하는 경우")
 	void testSearchByIdStartsWithBadParam2() {
-		Schedule schedule = saveSameScheduleSeatList(3);
-		String paramId = new StringBuilder().append(schedule.getId() + 1).append("_").toString();
+		Schedule schedule = saveReservedSeatMultiSeat(3);
+		String paramId = makeFindByScheduleParam(schedule.getId() + 1);
 		List<ReservedSeat> reservedSeats = reservedSeatRepository.searchByIdStartsWith(paramId);
 
 		assertThat(reservedSeats).isEmpty();
 	}
 
-	private String getParamId(Schedule schedule) {
-		return new StringBuilder().append(schedule.getId()).append("_").toString();
+	private String makeFindByScheduleParam(Long scheduleId) {
+		return new StringBuilder().append(scheduleId).append(ID_SEPARATOR).toString();
 	}
 }
