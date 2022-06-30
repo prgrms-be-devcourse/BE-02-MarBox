8406d78e62c698e02adee963983d941da252029e
yanJuicy <hanjub3@gmail.com>
2022-06-30 20:02:41 +0900

    feat(Message): INVALID_DATE_EXP_MSG 추가
    잘못된 날짜를 입력하면 해당 메시지 이용

--- src/main/java/prgrms/marco/be02marbox/domain/exception/custom/Message.java
+++ src/main/java/prgrms/marco/be02marbox/domain/exception/custom/Message.java
@@ -5,7 +5,8 @@ public enum Message {
 	DUPLICATE_NAME_EXP_MSG("이미 존재하는 이름입니다."),
 	INVALID_EMAIL_EXP_MSG("존재하지 않는 이메일 입니다."),
 	ACCESS_DENIED_EXP_MSG("API 접근 권한이 없습니다."),
-	INVALID_THEATER_EXP_MSG("존재하지 않는 극장 ID 입니다.");
+	INVALID_THEATER_EXP_MSG("존재하지 않는 극장 ID 입니다."),
+	INVALID_DATE_EXP_MSG("해당 날짜에 영화를 상영하지 않습니다.");
 
 	private final String message;
 
