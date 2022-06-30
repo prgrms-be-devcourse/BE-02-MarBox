c0b970706a2eae17bb966c59a1284481460dfc6e
hyuk0309 <bahngeh@naver.com>
2022-06-30 10:18:32 +0900

    refactor : 예외 메시지 수정
    기존 영어 예외 메시지 한국어로 수정
    예외 문자열 enum으로 관리

--- src/main/java/prgrms/marco/be02marbox/domain/exception/custom/Message.java
+++ src/main/java/prgrms/marco/be02marbox/domain/exception/custom/Message.java
@@ -5,7 +5,9 @@ public enum Message {
 	DUPLICATE_NAME_EXP_MSG("이미 존재하는 이름입니다."),
 	INVALID_EMAIL_EXP_MSG("존재하지 않는 이메일 입니다."),
 	ACCESS_DENIED_EXP_MSG("API 접근 권한이 없습니다."),
-	INVALID_THEATER_EXP_MSG("존재하지 않는 극장 ID 입니다.");
+	INVALID_THEATER_EXP_MSG("존재하지 않는 극장 ID 입니다."),
+	EMPTY_TOKEN_EXP_MSG("토큰은 필수 입니다."),
+	EMPTY_USERNAME_EXP_MSG("사용자 이름은 필수 입니다.");
 
 	private final String message;
 
