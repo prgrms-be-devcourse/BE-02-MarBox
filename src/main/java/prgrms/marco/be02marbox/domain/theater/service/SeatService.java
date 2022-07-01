5d60a9c4c8d68564b189a6c03947e268de9335d3
taesan <pawer02023@gmail.com>
2022-07-02 00:27:23 +0900

    chore: 주석 추가
    - 주석 추가

--- src/main/java/prgrms/marco/be02marbox/domain/theater/service/SeatService.java
+++ src/main/java/prgrms/marco/be02marbox/domain/theater/service/SeatService.java
@@ -16,6 +16,13 @@ public class SeatService {
 		this.seatRepository = seatRepository;
 	}
 
+	/**
+	 * 예약 가능한 좌석 정보 조회
+	 *
+	 * @param theaterRoomId 상영관 ID
+	 * @param reservedSeatIdList 예약 된 좌석리스트
+	 * @return 예약 가능한 좌석 리스트
+	 */
 	public List<Seat> findRemainSeats(Long theaterRoomId, List<Long> reservedSeatIdList) {
 		return seatRepository.findByTheaterRoomIdAndIdNotIn(theaterRoomId, reservedSeatIdList);
 	}
