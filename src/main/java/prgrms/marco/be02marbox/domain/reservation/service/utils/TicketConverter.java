4139470c4f241cbb466337361f8d906dd6ff9665
wisehero10 <godjw96@gmail.com>
2022-07-02 02:12:30 +0900

    feat : TicketConverter convertFromRequestCreateTicketToTicket() 추가

--- src/main/java/prgrms/marco/be02marbox/domain/reservation/service/utils/TicketConverter.java
+++ src/main/java/prgrms/marco/be02marbox/domain/reservation/service/utils/TicketConverter.java
@@ -3,6 +3,7 @@ package prgrms.marco.be02marbox.domain.reservation.service.utils;
 import org.springframework.stereotype.Component;
 
 import prgrms.marco.be02marbox.domain.reservation.Ticket;
+import prgrms.marco.be02marbox.domain.reservation.dto.RequestCreateTicket;
 import prgrms.marco.be02marbox.domain.reservation.dto.ResponseFindTicket;
 
 @Component
@@ -10,5 +11,9 @@ public class TicketConverter {
 
 	public ResponseFindTicket convertFromTicketToResponseFindTicket(Ticket ticket) {
 		return new ResponseFindTicket(ticket.getUser(), ticket.getSchedule(), ticket.getReservedAt());
+	}
+
+	public Ticket convertFromRequestCreateTicketToTicket(RequestCreateTicket request) {
+		return new Ticket(request.user(), request.schedule(), request.reservedAt());
 	}
 }
