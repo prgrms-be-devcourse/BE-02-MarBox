8354b3921f4928e1184d1d0ae7408d41b8d0130b
yanJuicy <hanjub3@gmail.com>
2022-07-01 14:24:07 +0900

    feat(Message): 에러 메시지 추가
    INVALID_MOVIE_EXP_MSG("존재하지 않는 영화 ID 입니다.")
    INVALID_THEATER_ROOM_EXP_MSG("존재하지 않는 상영관 ID 입니다.")

--- src/main/java/prgrms/marco/be02marbox/domain/exception/custom/Message.java
+++ src/main/java/prgrms/marco/be02marbox/domain/exception/custom/Message.java
@@ -7,7 +7,9 @@ public enum Message {
 	ACCESS_DENIED_EXP_MSG("API 접근 권한이 없습니다."),
 	INVALID_THEATER_EXP_MSG("존재하지 않는 극장 ID 입니다."),
 	NO_USER_TICKET_INFO_EXP_MSG("존재하지 않는 유저의 티켓 정보입니다."),
-	INVALID_DATE_EXP_MSG("해당 날짜에 영화를 상영하지 않습니다.");
+	INVALID_DATE_EXP_MSG("해당 날짜에 영화를 상영하지 않습니다."),
+	INVALID_MOVIE_EXP_MSG("존재하지 않는 영화 ID 입니다."),
+	INVALID_THEATER_ROOM_EXP_MSG("존재하지 않는 상영관 ID 입니다.");
 
 	private final String message;
 
