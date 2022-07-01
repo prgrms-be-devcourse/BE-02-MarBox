565d4a3d2e94b210bfce3f8a05be483b0db03f8c
yanJuicy <hanjub3@gmail.com>
2022-07-01 14:46:35 +0900

    feat(ScheduleConverter): Schedule 객체를 ResponseFindTime 객체로 변경

--- src/main/java/prgrms/marco/be02marbox/domain/theater/service/utils/ScheduleConverter.java
+++ src/main/java/prgrms/marco/be02marbox/domain/theater/service/utils/ScheduleConverter.java
@@ -1,7 +1,10 @@
 package prgrms.marco.be02marbox.domain.theater.service.utils;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.stereotype.Component;
 
@@ -9,6 +12,7 @@ import prgrms.marco.be02marbox.domain.movie.Movie;
 import prgrms.marco.be02marbox.domain.theater.Schedule;
 import prgrms.marco.be02marbox.domain.theater.TheaterRoom;
 import prgrms.marco.be02marbox.domain.theater.dto.RequestCreateSchedule;
+import prgrms.marco.be02marbox.domain.theater.dto.ResponseFindTime;
 
 @Component
 public class ScheduleConverter {
@@ -28,6 +32,24 @@ public class ScheduleConverter {
 			.map(schedule -> schedule.getStartTime().toLocalDate())
 			.distinct()
 			.toList();
+	}
+
+	public List<ResponseFindTime> convertFromScheduleInTheaterToResponseFindTimeList(
+		List<Schedule> schedules, Set<TheaterRoom> theaterRooms) {
+		List<ResponseFindTime> responseFindTimeSchedules = new ArrayList<>();
+
+		for (TheaterRoom theaterRoom : theaterRooms) {
+			List<LocalTime> startTimeListInTheater = schedules.stream()
+				.filter(schedule -> schedule.getTheaterRoom().equals(theaterRoom))
+				.map(schedule -> schedule.getStartTime().toLocalTime())
+				.toList();
+
+			responseFindTimeSchedules.add(
+				new ResponseFindTime(theaterRoom.getName(), theaterRoom.getTotalAmount(),
+					startTimeListInTheater));
+		}
+
+		return responseFindTimeSchedules;
 	}
 
 }
