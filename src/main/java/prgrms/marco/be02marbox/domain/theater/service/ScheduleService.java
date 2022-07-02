f8426877eccd8bdb9ff775b2fff250a07940dd36
yanJuicy <hanjub3@gmail.com>
2022-07-02 14:41:00 +0900

    refactor(ScheduleService): 안쓰는 import 제거

--- src/main/java/prgrms/marco/be02marbox/domain/theater/service/ScheduleService.java
+++ src/main/java/prgrms/marco/be02marbox/domain/theater/service/ScheduleService.java
@@ -9,7 +9,6 @@ import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
-import org.apache.tomcat.jni.Local;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
