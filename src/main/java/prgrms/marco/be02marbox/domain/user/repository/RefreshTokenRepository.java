8bc4ffa63e840f3ef52da13e382c8a7d1477bc4c
hyuk0309 <bahngeh@naver.com>
2022-07-04 17:13:25 +0900

    refactor(RefreshTokenRepository.java) : @Param 어노테이션 제거

--- src/main/java/prgrms/marco/be02marbox/domain/user/repository/RefreshTokenRepository.java
+++ src/main/java/prgrms/marco/be02marbox/domain/user/repository/RefreshTokenRepository.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import prgrms.marco.be02marbox.domain.user.RefreshToken;
 import prgrms.marco.be02marbox.domain.user.User;
@@ -14,5 +13,5 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
 	Optional<RefreshToken> findByUser(User user);
 
 	@Query("SELECT rt FROM RefreshToken rt JOIN FETCH rt.user WHERE rt.token=:refreshToken")
-	Optional<RefreshToken> findByToken(@Param("refreshToken") String refreshToken);
+	Optional<RefreshToken> findByToken(String refreshToken);
 }
