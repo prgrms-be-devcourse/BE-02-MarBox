d23518069c4fe7c7be8e117934b8487c4f0fd0d9
wisehero10 <godjw96@gmail.com>
2022-07-04 16:46:21 +0900

    refactor : 컨버터 수정

--- src/main/java/prgrms/marco/be02marbox/domain/reservation/service/utils/TicketConverter.java
+++ src/main/java/prgrms/marco/be02marbox/domain/reservation/service/utils/TicketConverter.java
@@ -1,22 +1,22 @@
 package prgrms.marco.be02marbox.domain.reservation.service.utils;
 
-import java.time.LocalDateTime;
-
 import org.springframework.stereotype.Component;
 
 import prgrms.marco.be02marbox.domain.reservation.Ticket;
 import prgrms.marco.be02marbox.domain.reservation.dto.ResponseFindTicket;
-import prgrms.marco.be02marbox.domain.theater.Schedule;
-import prgrms.marco.be02marbox.domain.user.User;
 
 @Component
 public class TicketConverter {
 
 	public ResponseFindTicket convertFromTicketToResponseFindTicket(Ticket ticket) {
-		return new ResponseFindTicket(ticket.getUser(), ticket.getSchedule(), ticket.getReservedAt());
-	}
-
-	public Ticket convertFromRequestCreateTicketToTicket(User user, Schedule schedule, LocalDateTime reservedAt) {
-		return new Ticket(user, schedule, reservedAt);
+		return new ResponseFindTicket(
+			ticket.getUser().getName(),
+			ticket.getSchedule().getMovie().getName(),
+			ticket.getSchedule().getMovie().getLimitAge().name(),
+			ticket.getSchedule().getTheaterRoom().getTheater().getName(),
+			ticket.getSchedule().getStartTime(),
+			ticket.getSchedule().getEndTime(),
+			ticket.getSchedule().getTheaterRoom().getName()
+		);
 	}
 }
