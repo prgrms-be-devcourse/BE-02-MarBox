b643bbe08c3da5e6bed9fa92e4ae3aa15176607b
wisehero10 <godjw96@gmail.com>
2022-07-05 10:54:35 +0900

    refactor : RequestCreateTicket 수정

--- src/main/java/prgrms/marco/be02marbox/domain/reservation/dto/RequestCreateTicket.java
+++ src/main/java/prgrms/marco/be02marbox/domain/reservation/dto/RequestCreateTicket.java
@@ -1,9 +1,13 @@
 package prgrms.marco.be02marbox.domain.reservation.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.validation.constraints.NotNull;
 
-public record RequestCreateTicket(@NotNull Long userId, @NotNull Long scheduleId, @NotNull LocalDateTime reservedAt) {
-
+public record RequestCreateTicket(
+	@NotNull Long userId,
+	@NotNull Long scheduleId,
+	@NotNull LocalDateTime reservedAt,
+	@NotNull List<Long> selectedSeatIds) {
 }
