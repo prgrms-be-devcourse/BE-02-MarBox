4e96dda27defb7ae079721804998165c66d0fef5
wisehero10 <godjw96@gmail.com>
2022-07-05 10:58:00 +0900

    refactor : N+1 문제 제거

--- src/main/java/prgrms/marco/be02marbox/domain/reservation/repository/TicketRepository.java
+++ src/main/java/prgrms/marco/be02marbox/domain/reservation/repository/TicketRepository.java
@@ -13,34 +13,45 @@ public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
 	@Override
 	@Query(
-		"select u.name, s.movie.name, s.movie.limitAge, s.theaterRoom.theater.name, s.theaterRoom.name, s.startTime, s.endTime, s.theaterRoom "
-			+ "from Ticket t left join  t.user u "
-			+ "left join  t.schedule s "
-			+ "left join s.movie m "
-			+ "left join s.theaterRoom thr "
-			+ "left join s.theaterRoom.theater th "
+		"select t "
+			+ "from Ticket t left join fetch  t.user u "
+			+ "left join fetch  t.schedule s "
+			+ "left join fetch s.movie m "
+			+ "left join fetch s.theaterRoom thr "
+			+ "left join fetch thr.theater th "
 			+ "where t.id = :ticketId"
 	)
 	Optional<Ticket> findById(@Param("ticketId") Long ticketId);
 
+	@Override
 	@Query(
-		"select  u.name, s.movie.name, s.movie.limitAge, s.theaterRoom.theater.name, s.theaterRoom.name, s.startTime, s.endTime "
-			+ "from Ticket t left join  t.user u "
-			+ "left join  t.schedule s "
-			+ "left join s.movie m "
-			+ "left join s.theaterRoom thr "
-			+ "left join s.theaterRoom.theater th "
+		"select t "
+			+ "from Ticket t left join fetch  t.user u "
+			+ "left join fetch  t.schedule s "
+			+ "left join fetch s.movie m "
+			+ "left join fetch s.theaterRoom thr "
+			+ "left join fetch thr.theater th "
+	)
+	List<Ticket> findAll();
+
+	@Query(
+		"select  t "
+			+ "from Ticket t left join fetch t.user u "
+			+ "left join fetch t.schedule s "
+			+ "left join fetch s.movie m "
+			+ "left join fetch s.theaterRoom thr "
+			+ "left join fetch thr.theater th "
 			+ "where t.user.id = :userId"
 	)
 	List<Ticket> findAllTicketByUserId(@Param("userId") Long userId);
 
 	@Query(
-		"select u.name, s.movie.name, s.movie.limitAge, s.theaterRoom.theater.name, s.theaterRoom.name, s.startTime, s.endTime "
-			+ "from Ticket t left join  t.user u "
-			+ "left join  t.schedule s "
-			+ "left join s.movie m "
-			+ "left join s.theaterRoom thr "
-			+ "left join s.theaterRoom.theater th "
+		"select t "
+			+ "from Ticket t left join fetch t.user u "
+			+ "left join  fetch t.schedule s "
+			+ "left join fetch s.movie m "
+			+ "left join fetch s.theaterRoom thr "
+			+ "left join fetch thr.theater th "
 			+ "where t.schedule.id = :scheduleId"
 	)
 	List<Ticket> findAllByScheduleId(@Param("scheduleId") Long scheduleId);
