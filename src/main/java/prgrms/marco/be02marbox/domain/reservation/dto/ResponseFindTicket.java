0b65cb3f87809077f0d933f423a2702c10f408b4
wisehero10 <godjw96@gmail.com>
2022-07-05 11:10:53 +0900

    refactor : 응답 데이터 수정

--- src/main/java/prgrms/marco/be02marbox/domain/reservation/dto/ResponseFindTicket.java
+++ src/main/java/prgrms/marco/be02marbox/domain/reservation/dto/ResponseFindTicket.java
@@ -1,14 +1,20 @@
 package prgrms.marco.be02marbox.domain.reservation.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import prgrms.marco.be02marbox.domain.movie.LimitAge;
+import prgrms.marco.be02marbox.domain.theater.dto.ResponseFindSeat;
 
 public record ResponseFindTicket(
 	String username,
 	String movieName,
-	String limitAge,
+	LimitAge limitAge,
 	String theaterName,
+	String theaterRoomName,
 	LocalDateTime startTime,
 	LocalDateTime endTime,
-	String theaterRoomName) {
+	List<ResponseFindSeat> seat
+) {
 
 }
