65558cc461ded4cda64889a08d8ad23dca2af9ac
taesan <pawer02023@gmail.com>
2022-07-11 00:03:13 +0900

    feat: 파일 업로드 S3 연동
    파일 업로드 S3 연동

--- src/test/java/prgrms/marco/be02marbox/domain/movie/service/MovieServiceTest.java
+++ src/test/java/prgrms/marco/be02marbox/domain/movie/service/MovieServiceTest.java
@@ -5,28 +5,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.mock.web.MockMultipartFile;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+
+import prgrms.marco.be02marbox.aws.S3Upload;
 import prgrms.marco.be02marbox.domain.movie.Genre;
 import prgrms.marco.be02marbox.domain.movie.LimitAge;
 import prgrms.marco.be02marbox.domain.movie.Movie;
-import prgrms.marco.be02marbox.domain.movie.dto.RequestCreateMovie;
 import prgrms.marco.be02marbox.domain.movie.repository.MovieRepository;
 import prgrms.marco.be02marbox.domain.movie.service.utils.MovieConverter;
 
 @DataJpaTest
-@Import({MovieService.class, MovieConverter.class})
+@Import({MovieService.class, MovieConverter.class, S3Upload.class, AmazonS3Client.class})
 class MovieServiceTest {
 
 	@Autowired
@@ -35,53 +34,27 @@ class MovieServiceTest {
 	@Autowired
 	MovieRepository movieRepository;
 
-	static RequestCreateMovie frozen;
-	static RequestCreateMovie notebook;
-	static RequestCreateMovie matrix;
-	static RequestCreateMovie tangled;
-	static RequestCreateMovie tazza;
-	static RequestCreateMovie aboutTime;
+	@PersistenceContext
+	EntityManager em;
 
-
-	@BeforeAll
-	static void beforeAll() {
-		frozen = new RequestCreateMovie("Frozen", LimitAge.CHILD, Genre.ANIMATION, 102,
-			new MockMultipartFile("fronzen.tmp", "fronzen.tmp", "tmp", "fronzen poster".getBytes()));
-		notebook = new RequestCreateMovie("NoteBook", LimitAge.CHILD, Genre.ROMANCE, 124,
-			new MockMultipartFile("notebook.tmp", "notebook.tmp", "tmp", "notebook poster".getBytes()));
-		matrix = new RequestCreateMovie("Matrix", LimitAge.CHILD, Genre.ACTION, 136,
-			new MockMultipartFile("matrix.tmp", "matrix.tmp", "tmp", "matrix poster".getBytes()));
-		tangled = new RequestCreateMovie("tangled", LimitAge.CHILD, Genre.ANIMATION, 148,
-			new MockMultipartFile("tangled.tmp", "tangled.tmp", "tmp", "tangled poster".getBytes()));
-		tazza = new RequestCreateMovie("tazza", LimitAge.ADULT, Genre.ACTION, 186,
-			new MockMultipartFile("tazza.tmp", "tazza.tmp", "tmp", "tazza poster".getBytes()));
-		aboutTime = new RequestCreateMovie("About Time", LimitAge.CHILD, Genre.ROMANCE, 124,
-			new MockMultipartFile("about_time.tmp", "about_time.tmp", "tmp", "about time poster".getBytes()));
-	}
-
-	@Test
-	@DisplayName("movie를 추가할 수 있다")
-	void testCreateMovie() throws IOException {
-		Long movieId = movieService.createMovie(frozen);
-		Optional<Movie> found = movieRepository.findById(movieId);
-		assertAll(
-			() -> assertThat(found.isPresent()).isTrue(),
-			() -> assertThat(found.get().getName()).isEqualTo(frozen.name()),
-			() -> assertThat(found.get().getLimitAge()).isEqualTo(frozen.limitAge()),
-			() -> assertThat(found.get().getGenre()).isEqualTo(frozen.genre()),
-			() -> assertThat(found.get().getRunningTime()).isEqualTo(frozen.runningTime())
-		);
-	}
+	Movie frozen = new Movie("Frozen", LimitAge.CHILD, Genre.ANIMATION, 102);
+	Movie notebook = new Movie("NoteBook", LimitAge.CHILD, Genre.ROMANCE, 124);
+	Movie matrix = new Movie("Matrix", LimitAge.CHILD, Genre.ACTION, 136);
+	Movie tangled = new Movie("tangled", LimitAge.CHILD, Genre.ANIMATION, 148);
+	Movie tazza = new Movie("tazza", LimitAge.ADULT, Genre.ACTION, 186);
+	Movie aboutTime = new Movie("About Time", LimitAge.CHILD, Genre.ROMANCE, 124);
 
 	@Test
 	@DisplayName("movie 리스트를 조회할 수 있다")
-	void testGetMovies() throws IOException {
-		movieService.createMovie(frozen);
-		movieService.createMovie(notebook);
-		movieService.createMovie(matrix);
-		movieService.createMovie(tangled);
-		movieService.createMovie(tazza);
-		movieService.createMovie(aboutTime);
+	void testGetMovies() {
+		movieRepository.save(frozen);
+		movieRepository.save(notebook);
+		movieRepository.save(matrix);
+		movieRepository.save(tangled);
+		movieRepository.save(tazza);
+		movieRepository.save(aboutTime);
+		em.flush();
+		em.clear();
 
 		List<Movie> movies1 = movieService.getMovies(0, 5);
 		List<Movie> movies2 = movieService.getMovies(1, 5);
@@ -90,7 +63,7 @@ class MovieServiceTest {
 		assertAll(
 			() -> assertThat(movies1).hasSize(5),
 			() -> assertThat(movies2).hasSize(1),
-			() -> assertThat(movies3).hasSize(0)
+			() -> assertThat(movies3).isEmpty()
 		);
 	}
 }
