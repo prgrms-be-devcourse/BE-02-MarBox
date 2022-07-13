ec8181b5786c548d0827b06148f329217a633a79
wisehero10 <godjw96@gmail.com>
2022-07-13 22:23:39 +0900

    refactor : ReservedSeatRepository QueryDSL 적용

--- src/main/java/prgrms/marco/be02marbox/domain/reservation/repository/ReservedSeatRepository.java
+++ src/main/java/prgrms/marco/be02marbox/domain/reservation/repository/ReservedSeatRepository.java
@@ -1,14 +1,8 @@
 package prgrms.marco.be02marbox.domain.reservation.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import prgrms.marco.be02marbox.domain.reservation.ReservedSeat;
 
-public interface ReservedSeatRepository extends JpaRepository<ReservedSeat, String> {
-	@Query("SELECT rs FROM ReservedSeat rs JOIN FETCH rs.seat WHERE rs.id LIKE CONCAT(:scheduleId, '\\_', '%')")
-	List<ReservedSeat> searchByScheduleIdStartsWith(@Param("scheduleId") Long scheduleId);
+public interface ReservedSeatRepository extends JpaRepository<ReservedSeat, String>, ReservedSeatRepositoryCustom {
 }
---
+++ src/main/java/prgrms/marco/be02marbox/domain/reservation/repository/ReservedSeatRepositoryCustomImpl.java
@@ -0,0 +1,29 @@
+package prgrms.marco.be02marbox.domain.reservation.repository;
+
+import static prgrms.marco.be02marbox.domain.reservation.QReservedSeat.*;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import prgrms.marco.be02marbox.domain.reservation.ReservedSeat;
+
+@Repository
+public class ReservedSeatRepositoryCustomImpl implements ReservedSeatRepositoryCustom {
+
+	private final JPAQueryFactory jpaQueryFactory;
+
+	public ReservedSeatRepositoryCustomImpl(JPAQueryFactory jpaQueryFactory) {
+		this.jpaQueryFactory = jpaQueryFactory;
+	}
+
+	@Override
+	public List<ReservedSeat> searchByScheduleIdStartsWith(Long scheduleId) {
+		return jpaQueryFactory.selectFrom(reservedSeat)
+			.leftJoin(reservedSeat.seat).fetchJoin()
+			.where(reservedSeat.id.like(scheduleId + "_" + "%"))
+			.fetch();
+	}
+}
